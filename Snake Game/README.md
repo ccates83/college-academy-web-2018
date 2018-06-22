79b92a9cf6692d36d8246af6cf125f07f77d329f
Connor M. Cates <cates.connor@gmail.com>
2018-06-21 23:12:51 -0400

    Snake Game
    Found a snake game tutorial online and used that to create the game -
    not sure if this is the best bc it shows more advanced style of coding
    that isn't how we are most likely gonna teach the kids - might have to
    refactor it / just add a lot of comments for the step by step
    explanations of everything

---
+++ Snake Game/README.md
@@ -0,0 +1,2 @@
+CITE: Maria Cristina Di Termine
+github: https://github.com/Mariacristina88/Snake-game
---
+++ Snake Game/js/setting.js
@@ -0,0 +1,10 @@
+// Initialize the settings for the game
+var mycanvas = document.getElementById('mycanvas');
+var ctx = mycanvas.getContext('2d');
+var snakeSize = 10; 
+var w = 350;
+var h = 350;
+var score = 0;
+var snake;
+var snakeSize = 10;
+var food;
